b9fd161222f42325853ccae30237aeac4327500f
zxhimv <zxhim.v@gmail.com>
2026-05-15 05:20:23 +0200

    Prepare v0.1.1 release housekeeping

--- bugos/__init__.py
+++ bugos/__init__.py
@@ -13,4 +13,4 @@ _SRC_PACKAGE = Path(__file__).resolve().parents[1] / "src" / "bugos"
 if _SRC_PACKAGE.is_dir():
     __path__.append(str(_SRC_PACKAGE))
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
--- src/bugos/__init__.py
+++ src/bugos/__init__.py
@@ -1,3 +1,3 @@
 """bugos: offline Bugcrowd operations helper."""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
